45507532c0636b61283be5901953d44eb84db2bf
IsaSam <isaacsamuel27@gmail.com>
2019-08-20 09:26:32 -0400

    Initial Commit

---
+++ Bibinetalkole/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Bibinetalkole
+//
+//  Created by Isaac Samuel on 8/20/19.
+//  Copyright © 2019 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
