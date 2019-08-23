92785b4d128a66d6b84f6d122099a160d9baef2a
IsaSam <isaacsamuel27@gmail.com>
2019-08-23 13:07:18 -0400

    collection view shows pictures added

--- Bibinetalkole/Controllers/ShowsViewController.swift
+++ Bibinetalkole/Controllers/ShowsViewController.swift
@@ -7,30 +7,96 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
-class ShowsViewController: UIViewController, UITableViewDataSource {
-    @IBOutlet weak var tableView: UITableView!
+
+class ShowsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var posts: [[String: Any]] = []
+    var pageID = 1
     var postsTitle: [[String: Any]] = []
     var postsContent: [[String: Any]] = []
-    var urlShows = ""
-    var pageID = 1
+    var postsEmbed: [[String: Any]] = []
+    var imgPosts: [[String: Any]] = []
+    var imgURLShare: String?
     
+    var urlShows = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
         
-        navigationItem.title = "Bibinetalkole"
-        
-        tableView.rowHeight = 200
-        tableView.estimatedRowHeight = 300
-        tableView.dataSource = self
-        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+ 
         fetchShows()
     }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+  //      let show = posts[indexPath.item]
+
+        do{
+            let titleDic = (posts as AnyObject).value(forKey: "title")
+            let embedDic = (posts as AnyObject).value(forKey: "_embedded")
+            
+            let titleDicString = titleDic as? [[String: Any]]
+            let embedDicString = embedDic as? [[String: Any]]
+            
+            self.postsTitle = titleDicString!
+            self.postsEmbed = embedDicString!
+        }
+        let postTitle = postsTitle[indexPath.row]
+        let postImage = postsEmbed[indexPath.row]
+        
+        let imgArray = (postImage as AnyObject).value(forKey: "wp:featuredmedia")
+        let mediaDetails = (imgArray as AnyObject).value(forKey: "media_details")
+        let sizes = (mediaDetails as AnyObject).value(forKey: "sizes")
+        
+        let encoded = postTitle["rendered"] as? String
+        print(encoded!)
+        ////
+        do{
+            let thumbnail =  (sizes as AnyObject).value(forKey: "thumbnail")
+            let dataDic = thumbnail as? [[String: Any]]
+            if dataDic != nil{
+                self.imgPosts = dataDic!
+                //          let remoteImageUrlString = imgPosts[indexPath.row]
+                //   }
+                ////
+                for images in imgPosts{
+                    let imageURL = images["source_url"] as? String
+                    print(imageURL!)
+                    if let imagePath = imageURL,
+                        let imgUrl = URL(string:  imagePath){
+                        cell.posterImageView.layer.borderColor = UIColor.white.cgColor
+                        cell.posterImageView.layer.borderWidth = 2.0
+                        cell.posterImageView.layer.cornerRadius = 2.0
+                        cell.posterImageView.clipsToBounds = true
+                        cell.posterImageView.af_setImage(withURL: imgUrl)
+                    }
+                    else{
+                    }
+                }
+            }else{}
+        }
+        ////
+    //    cell.titleLabel.text = encoded?.stringByDecodingHTMLEntities
+        
+        return cell
+    }
     
-    private func fetchShows(){
-        AyiboAPIManager.shared.get(url: "http://bibinetalkole.com/wp-json/wp/v2/posts?page=\(pageID)&categories=20") { (result, error) in
+    func fetchShows(){
+        AyiboAPIManager.shared.get(url: "http://bibinetalkole.com/wp-json/wp/v2/posts?page=\(pageID)&categories=20&_embed") { (result, error) in
             if error != nil{
                 let errorAlertController = UIAlertController(title: "Cannot Get Data", message: "The Internet connections appears to be offline", preferredStyle: .alert)
                 let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
@@ -41,105 +107,22 @@ class ShowsViewController: UIViewController, UITableViewDataSource {
                 return
             }
             if result != nil{
-             //   print(result!)
+                //   print(result!)
                 self.posts = result!
                 //autolayout engine from a background thread” error Fixed
                 DispatchQueue.main.async { //that allows the UI to update as soon as execution of thread function complete
-                    self.tableView.reloadData() // to tell table about new data
+                    self.collectionView.reloadData() // to tell table about new data
                 }
             }else{
                 print("nil")
             }
         }
-        
     }
-    
-    private func loadMoreShows(){
-        pageID = pageID + 1
-        AyiboAPIManager.shared.get(url: "http://bibinetalkole.com/wp-json/wp/v2/posts?page=\(pageID)&categories=20") { (result, error) in
-            
-            if error != nil{
-                let errorAlertController = UIAlertController(title: "On ne peut pas obtenir de données", message: "Les connexions Internet semblent être hors ligne", preferredStyle: .alert)
-                let cancelAction = UIAlertAction(title: "Réessayer", style: .cancel)
-                errorAlertController.addAction(cancelAction)
-                self.present(errorAlertController, animated: true)
-                return
-            }
-            if result != nil{
-                do{
-                    for item in result!
-                    {
-                        self.posts.append(item)
-                    }
-                    self.tableView.reloadData() // to tell table about new data
-                }
-            }else{
-                let errorAlertController = UIAlertController(title: "Sorry, No more posts", message: "Up the list", preferredStyle: .alert)
-                let cancelAction = UIAlertAction(title: "OK", style: .cancel)
-                errorAlertController.addAction(cancelAction)
-                self.present(errorAlertController, animated: true)
-            }
-        }
-        
-    }
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row + 1 == posts.count{
-            loadMoreShows()
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "showsCell", for: indexPath) as! ShowsViewCell
-        
-        do{
-            let titleDic = (posts as AnyObject).value(forKey: "title")
-            let contentDic = (posts as AnyObject).value(forKey: "content")
-            
-            let titleDicString = titleDic as? [[String: Any]]
-            let contentDicString = contentDic as? [[String: Any]]
-            
-            self.postsTitle = titleDicString!
-            self.postsContent = contentDicString!
-        }
-        let postTitle = postsTitle[indexPath.row]
-        let postContent = postsContent[indexPath.row]
-        
-        let encoded = postTitle["rendered"] as? String
-        let htmlTag =  postContent["rendered"] as! String
-        
-        cell.titleLabel.text = encoded?.stringByDecodingHTMLEntities
-        
-        let html2 = htmlTag.allStringsBetween(start: "iframe src=", end: "/iframe")
-        let input = String(describing: html2)
-        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        let matches = detector.matches(in: input, options: [], range: NSRange(location: 0, length: input.utf16.count))
-        for match in matches {
-            guard let range = Range(match.range, in: input) else { continue }
-            let urlYou = input[range]
-            if urlYou != ""{
-                urlShows = String(urlYou)
-                print(urlShows)
-                
-                if let url = URL(string: urlShows) {
-                    let request = URLRequest(url: url)
-                    cell.webView.load(request)
-                }
-                
-            }
-            else{
-            }
-        }
-        return cell
-    }
-    
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
---
+++ Bibinetalkole/ViewCell/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Bibinetalkole
+//
+//  Created by Isaac Samuel on 8/22/19.
+//  Copyright © 2019 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
