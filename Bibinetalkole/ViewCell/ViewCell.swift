e2ffbd5d8f6b3edbbc6cda89432ca3053ad69e66
IsaSam <isaacsamuel27@gmail.com>
2019-08-23 19:03:48 -0400

    autolayout fixed

---
+++ Bibinetalkole/ViewCell/ViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ViewCell.swift
+//  Bibinetalkole
+//
+//  Created by Isaac Samuel on 8/23/19.
+//  Copyright © 2019 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+}
