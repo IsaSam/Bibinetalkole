be384e49e650823fff1cc693a1a2c8ed615ef652
IsaSam <isaacsamuel27@gmail.com>
2019-08-26 15:49:47 -0400

    activityIndicator audioPlayer added

--- Bibinetalkole/Controllers/DetailsViewController.swift
+++ Bibinetalkole/Controllers/DetailsViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class DetailsViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
     
+    @IBOutlet weak var Activity: UIActivityIndicatorView!
     @IBOutlet weak var playAudio: WKWebView!
     
     @IBOutlet weak var contentLabel: UILabel!
@@ -27,6 +28,29 @@ class DetailsViewController: UIViewController {
         detailShow()
         playAudio.isOpaque = false
     }
+    func showActivityIndicator(show: Bool) {
+        if show {
+            Activity.startAnimating()
+        } else {
+            Activity.stopAnimating()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation:
+        WKNavigation!) {
+        showActivityIndicator(show: false)
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation
+        navigation: WKNavigation!) {
+        showActivityIndicator(show: true)
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation:
+        WKNavigation!, withError error: Error) {
+        showActivityIndicator(show: false)
+    }
+    
     func detailShow(){
         //for images
         let imgArray = (postImage as AnyObject).value(forKey: "wp:featuredmedia")
@@ -66,11 +90,14 @@ class DetailsViewController: UIViewController {
             let urlYou = input[range]
             if urlYou != ""{
                 urlShows = String(urlYou)
-          //      print(urlShows)
+                print(urlShows)
                 
                 if let url = URL(string: urlShows) {
+                    print("ok")
                     let request = URLRequest(url: url)
+                    print(request)
                     playAudio.load(request)
+                    playAudio.navigationDelegate = self as? WKNavigationDelegate
                     playAudio.scrollView.isScrollEnabled = false
                 }
             }
@@ -97,6 +124,10 @@ class DetailsViewController: UIViewController {
             contentLabel.text = ""
         }
     }
+   /* func webViewDidFinishLoad(_ : WKWebView) {
+        Activity.stopAnimating()
+        Activity.isHidden = true
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
