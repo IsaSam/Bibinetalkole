07d923978b0d05c76488cb6550bbf18c6929948d
IsaSam <isaacsamuel27@gmail.com>
2019-08-28 17:16:18 -0400

    about

---
+++ Bibinetalkole/Controllers/AboutUsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutUsViewController.swift
+//  Bibinetalkole
+//
+//  Created by Isaac Samuel on 8/27/19.
+//  Copyright © 2019 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
